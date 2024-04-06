985a856f0735c36506ca8887186b6fe5cacff120
Alejandro Coca <alxmcr@outlook.com>
2024-04-06 14:57:48 -0400

    colors

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,7 +1,8 @@
 export default function AppHeader() {
   return (
     <header>
-      <h3 className="text-3xl font-bold underline">eVolt</h3>
+      <h3 className="text-perano-300 text-3xl font-bold underline">eVolt</h3>
+      <h2 className="text-green-yellow-500">Computers</h2>
     </header>
   );
 }
