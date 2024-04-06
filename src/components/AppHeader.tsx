ac83c04b0b95754b00a919ee0fcf8f7f10575b9b
Alejandro Coca <alxmcr@outlook.com>
2024-04-06 14:50:35 -0400

    format code

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,5 +1,7 @@
-export default function AppHeader(){
-    return <header>
-        <h3 className="text-3xl font-bold underline">eVolt</h3>
+export default function AppHeader() {
+  return (
+    <header>
+      <h3 className="text-3xl font-bold underline">eVolt</h3>
     </header>
+  );
 }
