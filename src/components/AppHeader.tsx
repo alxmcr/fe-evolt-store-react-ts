f64cec103fbf27f3efe52bc4fb9a2efbfbf191aa
Alejandro Coca <alxmcr@outlook.com>
2024-04-06 15:05:07 -0400

    format code

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,7 +1,7 @@
 export default function AppHeader() {
   return (
     <header>
-      <h3 className="text-perano-300 text-3xl font-bold underline">eVolt</h3>
+      <h3 className="text-3xl font-bold text-perano-300 underline">eVolt</h3>
       <h2 className="text-green-yellow-500">Computers</h2>
       <button className="text-light-300 opacity-100">Filter</button>
     </header>
