3265785bd03e5a8ed7e7f8c2d0ea5f4c9e1f8842
Alejandro Coca <alxmcr@outlook.com>
2024-04-06 15:00:57 -0400

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,19 @@ export default {
       colors: {
         transparent: 'transparent',
         current: 'currentColor',
-        dark: '#000000',
-        light: '#ffffff',
+        white: {
+          50: '#ffffff',
+          100: '#efefef',
+          200: '#dcdcdc',
+          300: '#bdbdbd',
+          400: '#989898',
+          500: '#7c7c7c',
+          600: '#656565',
+          700: '#525252',
+          800: '#464646',
+          900: '#3d3d3d',
+          950: '#292929',
+        },
         perano: {
           50: '#eef3ff',
           100: '#dfe9ff',
