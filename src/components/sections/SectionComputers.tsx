300d012eba142a5594d491b0fae8060ead359d8e
Alejandro Coca <alxmcr@outlook.com>
2024-04-07 17:42:18 -0400

    wip

--- src/components/sections/SectionComputers.tsx
+++ src/components/sections/SectionComputers.tsx
@@ -5,14 +5,14 @@ import BoxGridProducts from '../boxes/BoxGridProducts';
 export default function SectionComputers() {
   return (
     <section>
-      <header>
+      <header className="flex items-center justify-between">
         <h2>Computers</h2>
-        <div>
-          <button>
+        <div className="flex items-center justify-between gap-2">
+          <button className="flex h-[1.625rem] w-[4.625rem] items-center gap-2 rounded-lg border-[1px] border-light-950 px-8">
             <Icon16x16Filter />
             <span>Filter</span>
           </button>
-          <button>
+          <button className="flex h-[1.625rem] w-[4.625rem] items-center gap-2 rounded-lg border-[1px] border-light-950 px-8">
             <Icon16x16Close />
             <span>Reset</span>
           </button>
