d2395fe151f739e5d3c645d24ffa79c3f9515294
Alejandro Coca <alxmcr@outlook.com>
2024-04-06 22:14:06 -0400

    icons

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,10 +1,22 @@
+import Icon18x18Search from './@icons/18x18/Icon18x18Search';
+import Icon26x26Cart from './@icons/26x26/Icon26x26Cart';
+import Icon26x26Heart from './@icons/26x26/Icon26x26Heart';
 import Icon26x26Thunderbolt from './@icons/26x26/Icon26x26Thunderbolt';
 
 export default function AppHeader() {
   return (
-    <header>
-      <a href="#">
-        <Icon26x26Thunderbolt className="text-green-400" />
+    <header className="flex flex-col gap-4">
+      <a href="#" className="text-green-400">
+        <Icon26x26Thunderbolt />
+      </a>
+      <a href="#" className="text-orange-500">
+        <Icon26x26Heart />
+      </a>
+      <a href="#" className="text-cyan-800">
+        <Icon26x26Cart />
+      </a>
+      <a href="#" className="text-cyan-800">
+        <Icon18x18Search />
       </a>
       <h3 className="text-perano-300 text-3xl font-bold underline">eVolt</h3>
       <h2 className="text-green-yellow-500">Computers</h2>
