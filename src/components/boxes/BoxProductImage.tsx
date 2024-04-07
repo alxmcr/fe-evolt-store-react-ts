7528a9ad66529f8c2e0a58026d388e708d102201
Alejandro Coca <alxmcr@outlook.com>
2024-04-07 15:24:36 -0400

    wip

--- src/components/boxes/BoxProductImage.tsx
+++ src/components/boxes/BoxProductImage.tsx
@@ -9,7 +9,9 @@ export default function BoxProductImage({
 }: Props) {
   return (
     <div className="h-[9.5rem] w-[9.75rem]">
-      <img src={`/product-assets/${photoFilename}`} alt={photoAlt} />
+      <div className="bg-perano-100">
+        <img src={`/product-assets/${photoFilename}`} alt={photoAlt} />
+      </div>
       <span className="text-sm">
         Designed by <a href="#">Freepik</a>
       </span>
