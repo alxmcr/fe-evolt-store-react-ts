53d07fc0d08a5e28978ca4b9a36dcc57c07e8422
Alejandro Coca <alxmcr@outlook.com>
2024-04-07 01:42:06 -0400

    wip

---
+++ src/@types/appTypes.ts
@@ -0,0 +1,12 @@
+export type ProductData = {
+  name: string;
+  price: string;
+  brand: string;
+  operatingSystem: string;
+  color: string;
+  model: string;
+  SKU: string;
+  memory: string;
+  urlPhoto: string;
+  screenSize: string;
+};
---
+++ src/mocks/mock-products-computers-data.ts
@@ -0,0 +1,144 @@
+import { ProductData } from '../@types/appTypes';
+
+export const product001: ProductData = {
+  name: 'Apple MacBook Air M2',
+  price: '$1199',
+  brand: 'Apple',
+  operatingSystem: 'macOS Monterey',
+  color: 'Space Gray',
+  model: 'M2',
+  SKU: 'MKW43LL/A',
+  memory: '8GB RAM, 256GB SSD',
+  urlPhoto: '[Image of Apple MacBook Air M2]',
+  screenSize: '13.6-inch',
+};
+
+export const product002: ProductData = {
+  name: 'Microsoft Surface Laptop Studio',
+  price: '$1599',
+  brand: 'Microsoft',
+  operatingSystem: 'Windows 11 Home',
+  color: 'Platinum',
+  model: 'i7-1185G7',
+  SKU: '206X00PT',
+  memory: '16GB RAM, 512GB SSD',
+  urlPhoto: '[Image of Microsoft Surface Laptop Studio]',
+  screenSize: '14.4-inch',
+};
+
+export const product003: ProductData = {
+  name: 'ASUS ROG Strix G15 Advantage Edition',
+  price: '$1799',
+  brand: 'ASUS',
+  operatingSystem: 'Windows 11 Home',
+  color: 'Black',
+  model: 'Ryzen 9 6900HX',
+  SKU: 'G614JM-XB99Q',
+  memory: '16GB RAM, 1TB SSD',
+  urlPhoto: '[Image of ASUS ROG Strix G15 Advantage Edition]',
+  screenSize: '15.6-inch',
+};
+
+export const product004: ProductData = {
+  name: 'Acer Chromebook Spin 713',
+  price: '$699',
+  brand: 'Acer',
+  operatingSystem: 'Chrome OS',
+  color: 'Silver',
+  model: 'i5-1235U',
+  SKU: 'LX728T-ER726',
+  memory: '8GB RAM, 256GB SSD',
+  urlPhoto: '[Image of Acer Chromebook Spin 713]',
+  screenSize: '13.5-inch',
+};
+
+export const product005: ProductData = {
+  name: 'Lenovo IdeaPad Flex 5 14',
+  price: '$799',
+  brand: 'Lenovo',
+  operatingSystem: 'Windows 11 Home',
+  color: 'Grey',
+  model: 'Ryzen 5 5500U',
+  SKU: '82LM00QBUS',
+  memory: '16GB RAM, 256GB SSD',
+  urlPhoto: '[Image of Lenovo IdeaPad Flex 5 14]',
+  screenSize: '14-inch',
+};
+
+export const product006: ProductData = {
+  name: 'HP Spectre x360 14',
+  price: '$1299',
+  brand: 'HP',
+  operatingSystem: 'Windows 11 Home',
+  color: 'Nightfall Teal',
+  model: 'i7-1260P',
+  SKU: '8NR93AV_16',
+  memory: '16GB RAM, 1TB SSD',
+  urlPhoto: '[Image of HP Spectre x360 14]',
+  screenSize: '14-inch',
+};
+
+export const product007: ProductData = {
+  name: 'Dell XPS 13 Plus',
+  price: '$999',
+  brand: 'Dell',
+  operatingSystem: 'Windows 11 Pro',
+  color: 'Platinum White',
+  model: 'i7-1250U',
+  SKU: '9305-0004',
+  memory: '16GB RAM, 512GB SSD',
+  urlPhoto: '[Image of Dell XPS 13 Plus]',
+  screenSize: '13.4-inch',
+};
+
+export const product008: ProductData = {
+  name: 'Samsung Galaxy Book 2 Pro 360',
+  price: '$1499',
+  brand: 'Samsung',
+  operatingSystem: 'Windows 11 Home',
+  color: 'Mystic Silver',
+  model: 'i7-1260P',
+  SKU: 'NP950QDB-KA3US',
+  memory: '16GB RAM, 1TB SSD',
+  urlPhoto: '[Image of Samsung Galaxy Book 2 Pro 360]',
+  screenSize: '13.3-inch',
+};
+
+export const product009: ProductData = {
+  name: 'MSI GF65 Thin',
+  price: '$1099',
+  brand: 'MSI',
+  operatingSystem: 'Windows 11 Home',
+  color: 'Black',
+  model: 'RTX 3060',
+  SKU: '11SCXR-203US',
+  memory: '16GB RAM, 512GB SSD',
+  urlPhoto: '[Image of MSI GF65 Thin]',
+  screenSize: '15.6-inch',
+};
+
+export const product010: ProductData = {
+  name: 'Apple iMac 24-inch',
+  price: '$1299',
+  brand: 'Apple',
+  operatingSystem: 'macOS Monterey',
+  color: 'Blue',
+  model: 'M1',
+  SKU: 'MGJ73LL/A',
+  memory: '8GB RAM, 256GB SSD',
+  urlPhoto: '[Image of Apple iMac 24-inch]',
+  screenSize: '24-inch',
+};
+
+export const MOCK_DATA_COMPUTERS = [
+  product001,
+  product002,
+  product003,
+  product004,
+  product005,
+  product006,
+  product007,
+  product008,
+  product009,
+  product010,
+];
