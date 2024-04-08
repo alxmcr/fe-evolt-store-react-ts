114a360d128d4eafc57e23893d1a1cb6e8e62e19
Alejandro Coca <alxmcr@outlook.com>
2024-04-08 00:51:04 -0400

    wip

--- src/components/asides/AsideProductsFilter.tsx
+++ src/components/asides/AsideProductsFilter.tsx
@@ -1,5 +1,7 @@
 export default function AsideProductsFilter() {
   return (
-    <aside className="hidden rounded-xl bg-white lg:block">hola!!!!</aside>
+    <aside className="hidden rounded-xl bg-white lg:block lg:w-[18.375rem]">
+      hola!!!!
+    </aside>
   );
 }
