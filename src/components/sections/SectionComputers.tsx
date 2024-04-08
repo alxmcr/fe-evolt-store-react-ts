c8eefbf03e0437106f1e507f81a5c541195ed394
Alejandro Coca <alxmcr@outlook.com>
2024-04-07 23:46:44 -0400

    wip

--- src/components/sections/SectionComputers.tsx
+++ src/components/sections/SectionComputers.tsx
@@ -5,15 +5,19 @@ import BoxGridProducts from '../boxes/BoxGridProducts';
 export default function SectionComputers() {
   return (
     <section className="flex flex-col gap-4">
-      <header className="flex items-center justify-between">
-        <h2 className="font-bold text-xl">Computers</h2>
+      <header className="flex items-center justify-between gap-2">
+        <h2 className="text-xl font-bold">Computers</h2>
         <div className="flex items-center justify-between gap-2">
-          <button className="flex min-h-[1.875rem] min-w-[5.125rem] items-center gap-2 rounded-lg border-[1px] border-light-950 px-4">
-            <Icon16x16Filter />
+          <button className="flex min-h-[1.875rem] max-w-[5.125rem] items-center gap-2 rounded-lg border-[1px] border-light-950 px-4">
+            <span className="w-[1rem]">
+              <Icon16x16Filter />
+            </span>
             <span className="text-[.75rem]">Filter</span>
           </button>
-          <button className="flex min-h-[1.875rem] min-w-[5.125rem] items-center gap-2 rounded-lg border-[1px] border-light-950 px-4">
-            <Icon16x16Close />
+          <button className="flex min-h-[1.875rem] max-w-[5.125rem] items-center gap-2 rounded-lg border-[1px] border-light-950 px-4">
+            <span className="w-[1rem]">
+              <Icon16x16Close />
+            </span>
             <span className="text-[.75rem]">Reset</span>
           </button>
         </div>
