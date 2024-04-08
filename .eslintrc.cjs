602352bc5db36fae5636108a44e3e1c510507cd1
Alejandro Coca <alxmcr@outlook.com>
2024-04-07 23:51:12 -0400

    wip

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,23 @@
 module.exports = {
   root: true,
+  settings: {
+    tailwindcss: {
+      config: './tailwind.config.js',
+    },
+  },
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:tailwindcss/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   overrides: [
+    {
+      files: ['.eslintrc.{js,cjs}', '*.ts', '*.tsx', '*.js'],
+      parser: '@typescript-eslint/parser',
+    },
     {
       files: ['.eslintrc.{js,cjs}', '*.ts', '*.tsx', '*.js'],
       parser: '@typescript-eslint/parser',
