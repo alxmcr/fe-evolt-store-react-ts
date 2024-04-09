effc5854ebb8a5f2ea4fd2fd6d13b22360d782f8
Alejandro Coca <alxmcr@outlook.com>
2024-04-09 17:26:59 -0400

    wip

--- src/components/cards/CardPriceRangeSlider.tsx
+++ src/components/cards/CardPriceRangeSlider.tsx
@@ -1,15 +1,6 @@
-import React from 'react';
-import { formatNumber } from '../../helpers/helpers-format';
-import FormFilterByRangePrices from '../forms/FormFilterByRangePrices';
+import BoxFormFilterByRangePrices from '../boxes/BoxFormFilterByRangePrices';
 
 export default function CardPriceRangeSlider() {
-  const MIN_VALUE = 1;
-  const MAX_VALUE = 1100;
-  const START_MIN_VALUE = 500;
-  const START_MAX_VALUE = 900;
-  const [minPrice, setMinPrice] = React.useState(START_MIN_VALUE);
-  const [maxPrice, setMaxPrice] = React.useState(START_MAX_VALUE);
-
   return (
     <article className="flex flex-col gap-3">
       <header className="flex flex-col gap-1">
@@ -17,21 +8,7 @@ export default function CardPriceRangeSlider() {
           Pricing
         </h4>
       </header>
-      <div className="flex flex-col gap-2">
-        <div className="flex gap-2 text-perano-300">
-          <span>$ {formatNumber(minPrice)}</span>
-          <span>-</span>
-          <span>$ {formatNumber(maxPrice)}</span>
-        </div>
-        <FormFilterByRangePrices
-          minValue={MIN_VALUE}
-          maxValue={MAX_VALUE}
-          minPrice={minPrice}
-          maxPrice={maxPrice}
-          setMinPrice={setMinPrice}
-          setMaxPrice={setMaxPrice}
-        />
-      </div>
+      <BoxFormFilterByRangePrices />
     </article>
   );
 }
