c4847866b39dd1f5c01131265856ccb6daceb03e
Alejandro Coca <alxmcr@outlook.com>
2024-04-10 16:40:35 -0400

    wip

--- src/mocks/mock-computers-storages.ts
+++ src/mocks/mock-computers-storages.ts
@@ -1,24 +1,24 @@
 import { StorageData } from '../@types/appTypes';
 
-export const storage001: StorageData = {
+const storage001: StorageData = {
   id: 'storage0001',
   capacity: '128GB SSD + 128GB Micro SD',
 };
-export const storage002: StorageData = {
+const storage002: StorageData = {
   id: 'storage0002',
   capacity: '256GB SSD',
 };
 
-export const storage003: StorageData = {
+const storage003: StorageData = {
   id: 'storage0002',
   capacity: '512GB SSD',
 };
 
-export const storage004: StorageData = {
+const storage004: StorageData = {
   id: 'storage0004',
   capacity: '1TB SSD',
 };
-export const storage005: StorageData = {
+const storage005: StorageData = {
   id: 'storage0005',
   capacity: '2TB SSD',
 };
