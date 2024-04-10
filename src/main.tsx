5158974df2409f30187528fd7ea5d6b0032d9952
Alejandro Coca <alxmcr@outlook.com>
2024-04-10 19:30:45 -0400

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './index.css';
 import { routes } from './router/routes.tsx';
+import ProductFilterProvider from './providers/ProductFilterProvider/index.tsx';
 
 const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ProductFilterProvider>
+      <RouterProvider router={router} />
+    </ProductFilterProvider>
   </React.StrictMode>,
 );
