9f60ee05e7aa950dcd16f93707b0a9458620e79a
Alejandro Coca <alxmcr@outlook.com>
2024-04-10 16:14:01 -0400

    wip

--- src/components/forms-formfields/FormFieldBrand.tsx
+++ src/components/forms-formfields/FormFieldBrand.tsx
@@ -19,8 +19,8 @@ export default function FormFieldBrand({
         className="peer/draft hidden size-10"
       />
       <span
-        className="flex items-center gap-2 text-perano-500 before:flex before:size-5 before:items-center before:justify-center before:rounded-full before:border before:border-solid before:content-[''] peer-checked/draft:before:bg-[url(icons/16x16/Icon16x16Check.svg)] peer-checked/draft:before:bg-center
-        peer-checked/draft:before:bg-no-repeat peer-checked/draft:before:text-sky-500"
+        className="flex items-center gap-2 text-perano-500 before:flex before:size-5 before:items-center before:justify-center before:rounded-full before:border before:border-solid before:border-perano-500 before:content-[''] peer-checked/draft:before:bg-perano-500 peer-checked/draft:before:bg-[url(icons/16x16/Icon16x16Check.svg)]
+        peer-checked/draft:before:bg-center peer-checked/draft:before:bg-no-repeat peer-checked/draft:before:text-sky-500"
       >
         {labelText}
       </span>
