5abde5204943e8ccf1f678d593fc9d3a19e81962
Alejandro Coca <alxmcr@outlook.com>
2024-04-10 16:22:27 -0400

    wip

--- src/components/forms-formfields/FormFieldBrand.tsx
+++ src/components/forms-formfields/FormFieldBrand.tsx
@@ -8,10 +8,7 @@ export default function FormFieldBrand({
   htmlFor = '',
 }: Props) {
   return (
-    <label
-      htmlFor={htmlFor}
-      className={`flex items-center gap-x-2 text-lg font-semibold text-[#25297B]`}
-    >
+    <label htmlFor={htmlFor} className={`flex items-center gap-x-2 text-lg`}>
       <input
         type="checkbox"
         name={htmlFor}
@@ -19,8 +16,8 @@ export default function FormFieldBrand({
         className="peer/draft hidden size-10"
       />
       <span
-        className="flex items-center gap-2 text-perano-500 before:flex before:size-5 before:items-center before:justify-center before:rounded-full before:border before:border-solid before:border-perano-500 before:content-[''] peer-checked/draft:before:bg-perano-500 peer-checked/draft:before:bg-[url(icons/16x16/Icon16x16Check.svg)]
-        peer-checked/draft:before:bg-center peer-checked/draft:before:bg-no-repeat peer-checked/draft:before:text-sky-500"
+        className="flex items-center gap-2 before:flex before:size-5 before:items-center before:justify-center before:rounded-full before:border before:border-solid before:border-perano-500 before:content-[''] peer-checked/draft:text-perano-500  peer-checked/draft:before:bg-perano-500
+        peer-checked/draft:before:bg-[url(icons/16x16/Icon16x16Check.svg)] peer-checked/draft:before:bg-center peer-checked/draft:before:bg-no-repeat"
       >
         {labelText}
       </span>
