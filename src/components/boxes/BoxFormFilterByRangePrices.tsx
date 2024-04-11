fe01db020c966d9a39d3f2c8592094ae20be0c63
Alejandro Coca <alxmcr@outlook.com>
2024-04-11 18:13:44 -0400

    wip

--- src/components/boxes/BoxFormFilterByRangePrices.tsx
+++ src/components/boxes/BoxFormFilterByRangePrices.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { formatNumber } from '../../helpers/helpers-format';
 import FormFilterByRangePrices from '../forms/FormFilterByRangePrices';
 
-export default function BoxFormFilterByRangePrices() {
-  const MIN_VALUE = 1;
-  const MAX_VALUE = 1100;
-  const START_MIN_VALUE = 500;
-  const START_MAX_VALUE = 900;
-  const [minPrice, setMinPrice] = React.useState(START_MIN_VALUE);
-  const [maxPrice, setMaxPrice] = React.useState(START_MAX_VALUE);
+type Props = {
+  minValue: number;
+  maxValue: number;
+  startMinValue: number;
+  startMaxValue: number;
+};
+
+export default function BoxFormFilterByRangePrices({
+  minValue = 0,
+  maxValue = 0,
+  startMinValue = 0,
+  startMaxValue = 0,
+}: Props) {
+  const [minPrice, setMinPrice] = React.useState(startMinValue);
+  const [maxPrice, setMaxPrice] = React.useState(startMaxValue);
 
   return (
     <div className="flex flex-col gap-2">
@@ -18,8 +26,8 @@ export default function BoxFormFilterByRangePrices() {
         <span>$ {formatNumber(maxPrice)}</span>
       </div>
       <FormFilterByRangePrices
-        minValue={MIN_VALUE}
-        maxValue={MAX_VALUE}
+        minValue={minValue}
+        maxValue={maxValue}
         minPrice={minPrice}
         maxPrice={maxPrice}
         setMinPrice={setMinPrice}
