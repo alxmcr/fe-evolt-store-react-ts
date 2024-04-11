d479391a9c66892159415b9403f618c79523160e
Alejandro Coca <alxmcr@outlook.com>
2024-04-11 18:01:59 -0400

    wip

--- src/components/sections/SectionComputers.tsx
+++ src/components/sections/SectionComputers.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import Icon16x16Close from '../@icons/16x16/Icon16x16Close';
-import Icon16x16Filter from '../@icons/16x16/Icon16x16Filter';
+import { TagFilter } from '../../@types/filterTypes';
 import AsideProductsFilter from '../asides/AsideProductsFilter';
 import BoxGridProducts from '../boxes/BoxGridProducts';
-import { TagFilter } from '../../@types/filterTypes';
 
 export default function SectionComputers() {
   const [brandsSelected, setBrandsSelected] = React.useState<TagFilter[]>([]);
@@ -16,23 +14,7 @@ export default function SectionComputers() {
 
   return (
     <section className="flex flex-col gap-4">
-      <header className="flex items-center justify-between gap-2">
-        <h2 className="text-xl font-bold">Computers</h2>
-        <div className="flex items-center justify-between gap-2">
-          <button className="flex min-h-[1.875rem] max-w-[5.125rem] items-center gap-2 rounded-lg border border-light-950 px-4">
-            <span className="w-4">
-              <Icon16x16Filter />
-            </span>
-            <span className="text-[.75rem]">Filter</span>
-          </button>
-          <button className="flex min-h-[1.875rem] max-w-[5.125rem] items-center gap-2 rounded-lg border border-light-950 px-4">
-            <span className="w-4">
-              <Icon16x16Close />
-            </span>
-            <span className="text-[.75rem]">Reset</span>
-          </button>
-        </div>
-      </header>
+      <h2 className="text-xl font-bold">Computers</h2>
       <div className="lg:relative lg:flex lg:gap-4">
         <AsideProductsFilter
           setBrandsSelected={setBrandsSelected}
