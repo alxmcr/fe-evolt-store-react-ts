14db14d29d3e215167b0976a590398d28bd57bd7
Alejandro Coca <alxmcr@outlook.com>
2024-04-11 19:59:12 -0400

    wip

--- src/components/asides/AsideProductsFilter.tsx
+++ src/components/asides/AsideProductsFilter.tsx
@@ -1,65 +1,51 @@
 import React from 'react';
-import { TagFilter } from '../../@types/filterTypes';
 import { MOCK_BLUETOOTHS } from '../../mocks/mock-laptops-bluetooth';
 import { MOCK_BRANDS } from '../../mocks/mock-laptops-brands';
 import { MOCK_STORAGES } from '../../mocks/mock-laptops-storages';
+import { ProductFilterCriteriasContext } from '../../providers/ProductFilterCriteriasProvider/ProductFilterCriteriasContext';
 import BoxFormFilterByRangePrices from '../boxes/BoxFormFilterByRangePrices';
 import CardAsideBase from '../cards/CardAsideBase';
 import GroupCheckboxesBluetooths from '../groups-checkboxes/GroupCheckboxesBluetooths';
 import GroupCheckboxesBrands from '../groups-checkboxes/GroupCheckboxesBrands';
 import GroupCheckboxesStorages from '../groups-checkboxes/GroupCheckboxesStorages';
 
-type Props = {
-  minValue: number;
-  maxValue: number;
-  minPrice: number;
-  maxPrice: number;
-  setMinPrice: React.Dispatch<React.SetStateAction<number>>;
-  setMaxPrice: React.Dispatch<React.SetStateAction<number>>;
-  setBrandsSelected: React.Dispatch<React.SetStateAction<TagFilter[]>>;
-  setBluetoothsSelected: React.Dispatch<React.SetStateAction<TagFilter[]>>;
-  setStoragesSelected: React.Dispatch<React.SetStateAction<TagFilter[]>>;
-};
+export default function AsideProductsFilter() {
+  const contextProductFilterCriterias = React.useContext(
+    ProductFilterCriteriasContext,
+  );
 
-export default function AsideProductsFilter({
-  minValue = 0,
-  maxValue = 0,
-  minPrice = 0,
-  maxPrice = 0,
-  setMinPrice,
-  setMaxPrice,
-  setBrandsSelected,
-  setBluetoothsSelected,
-  setStoragesSelected,
-}: Props) {
   return (
     <aside className="hidden rounded-xl bg-white p-4 lg:sticky lg:flex lg:h-[810px] lg:w-2/6 lg:flex-col lg:gap-4">
       <CardAsideBase title="Pricing">
         <BoxFormFilterByRangePrices
-          minValue={minValue}
-          maxValue={maxValue}
-          minPrice={minPrice}
-          maxPrice={maxPrice}
-          setMinPrice={setMinPrice}
-          setMaxPrice={setMaxPrice}
+          minValue={contextProductFilterCriterias.minValue}
+          maxValue={contextProductFilterCriterias.maxValue}
+          minPrice={contextProductFilterCriterias.minPrice}
+          maxPrice={contextProductFilterCriterias.maxPrice}
+          setMinPrice={contextProductFilterCriterias.setMinPrice}
+          setMaxPrice={contextProductFilterCriterias.setMaxPrice}
         />
       </CardAsideBase>
       <CardAsideBase title="Brands">
         <GroupCheckboxesBrands
           brands={MOCK_BRANDS}
-          setBrandsSelected={setBrandsSelected}
+          setBrandsSelected={contextProductFilterCriterias.setBrandsSelected}
         />
       </CardAsideBase>
       <CardAsideBase title="Storage">
         <GroupCheckboxesStorages
           storages={MOCK_STORAGES}
-          setStoragesSelected={setStoragesSelected}
+          setStoragesSelected={
+            contextProductFilterCriterias.setStoragesSelected
+          }
         />
       </CardAsideBase>
       <CardAsideBase title="Bluetooth">
         <GroupCheckboxesBluetooths
           bluetooths={MOCK_BLUETOOTHS}
-          setBluetoothsSelected={setBluetoothsSelected}
+          setBluetoothsSelected={
+            contextProductFilterCriterias.setBluetoothsSelected
+          }
         />
       </CardAsideBase>
     </aside>
