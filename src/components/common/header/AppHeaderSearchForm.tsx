b25f8635f1a6c14fa366347bd4e1407c89bc50a6
Alejandro Coca <alxmcr@outlook.com>
2024-04-12 19:52:02 -0400

    wip

--- src/components/common/header/AppHeaderSearchForm.tsx
+++ src/components/common/header/AppHeaderSearchForm.tsx
@@ -2,7 +2,7 @@ import Icon18x18Search from '../../@icons/18x18/Icon18x18Search';
 
 export default function AppHeaderSearchForm() {
   return (
-    <form className="hidden rounded-2xl bg-white px-[.25rem] py-[.1875rem] md:flex md:h-[2.25rem] md:w-[19.75rem]">
+    <form className="hidden rounded-2xl bg-white px-[.25rem] py-[.1875rem] md:flex md:h-9 md:w-[19.75rem]">
       <input
         type="text"
         placeholder="Enter your product name"
