bd3e2a73e7c20de906f68c08861349b63034f7c0
Alejandro Coca <alxmcr@outlook.com>
2024-04-12 18:04:25 -0400

    wip

--- src/components/boxes/BoxFilterActions.tsx
+++ src/components/boxes/BoxFilterActions.tsx
@@ -9,7 +9,7 @@ export default function BoxFilterActions() {
   );
 
   return (
-    <div className="flex flex-wrap items-center gap-4 rounded-lg bg-light-50 p-4">
+    <div className="flex flex-col flex-wrap justify-start gap-4 rounded-lg bg-light-50 p-4">
       <header className="flex items-center gap-4">
         <h4 className="font-bold uppercase">Filters</h4>
         <button className="flex min-h-[1.875rem] max-w-[5.125rem] items-center gap-2 rounded-lg border border-light-950 px-4">
