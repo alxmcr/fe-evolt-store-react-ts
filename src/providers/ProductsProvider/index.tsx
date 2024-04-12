920c683d674e6f2063446ab8047998811522af29
Alejandro Coca <alxmcr@outlook.com>
2024-04-11 23:25:46 -0400

    wip

--- src/providers/ProductsProvider/index.tsx
+++ src/providers/ProductsProvider/index.tsx
@@ -4,6 +4,13 @@ import useProducts from '../../hooks/useProducts';
 import { ProductsContext } from './ProductsContext';
 import { LoadingStates } from '../../@types/serviceTypes';
 import { ProductData } from '../../@types/appTypes';
+import {
+  findAllBluetooths,
+  findAllBrands,
+  findAllStorages,
+  getMaxPricePossible,
+  getMinPricePossible,
+} from '../../helpers/helpers-products';
 
 type Props = {
   children: React.ReactNode;
@@ -23,6 +30,19 @@ export default function ProductsProvider({ children }: Props) {
   React.useEffect(() => {
     if (LoadingStates.SUCCESS === statusProducts) {
       setProductsFiltered(products);
+      // Extract data
+      const min = getMinPricePossible(products);
+      const max = getMaxPricePossible(products);
+      const brands = findAllBrands(products);
+      const storages = findAllStorages(products);
+      const bluetooths = findAllBluetooths(products);
+      console.log({
+        max,
+        min,
+        brands,
+        storages,
+        bluetooths,
+      });
     }
   }, [statusProducts, products]);
 
