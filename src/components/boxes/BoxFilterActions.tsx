07cc8e0becd0cca441a227c8531ecdce3132969e
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2024-04-12 18:57:30 -0400

    feature/reset-filter-button-init (#21)

--- src/components/boxes/BoxFilterActions.tsx
+++ src/components/boxes/BoxFilterActions.tsx
@@ -8,11 +8,18 @@ export default function BoxFilterActions() {
     ProductFilterCriteriasContext,
   );
 
+  const clearTagsFilter = () => {
+    contextProductFilterCriterias.setTagsFilter([]);
+  };
+
   return (
     <div className="flex flex-col flex-wrap justify-start gap-4 rounded-lg bg-light-50 p-4">
       <header className="flex items-center gap-4">
         <h4 className="font-bold uppercase">Filters</h4>
-        <button className="flex min-h-[1.875rem] max-w-[5.125rem] items-center gap-2 rounded-lg border border-light-950 px-4">
+        <button
+          className="flex min-h-[1.875rem] max-w-[5.125rem] items-center gap-2 rounded-lg border border-light-950 px-4"
+          onClick={clearTagsFilter}
+        >
           <span className="w-4">
             <Icon16x16Close />
           </span>
