226d701dc645fa6b700e2f723d0e606251c762b5
Alejandro Coca <alxmcr@outlook.com>
2024-04-12 00:48:18 -0400

    wip

--- src/providers/ProductsProvider/index.tsx
+++ src/providers/ProductsProvider/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { ProductData } from '../../@types/appTypes';
+import {
+  BluetoothData,
+  BrandData,
+  ProductData,
+  StorageData,
+} from '../../@types/appTypes';
 import { ProductsContextData } from '../../@types/providerTypes';
 import { LoadingStates } from '../../@types/serviceTypes';
 import {
   findAllBluetooths,
   findAllBrands,
   findAllStorages,
+  getMaxPricePossible,
+  getMinPricePossible,
 } from '../../helpers/helpers-products';
 import useProducts from '../../hooks/useProducts';
 import { ProductsContext } from './ProductsContext';
@@ -15,6 +22,11 @@ type Props = {
 };
 
 export default function ProductsProvider({ children }: Props) {
+  const [minPrice, setMinPrice] = React.useState(0);
+  const [maxPrice, setMaxPrice] = React.useState(0);
+  const [brands, setBrands] = React.useState<BrandData[]>([]);
+  const [storages, setStorages] = React.useState<StorageData[]>([]);
+  const [bluetooths, setBluetooths] = React.useState<BluetoothData[]>([]);
   const [productsFiltered, setProductsFiltered] = React.useState<ProductData[]>(
     [],
   );
@@ -23,36 +35,23 @@ export default function ProductsProvider({ children }: Props) {
   const value: ProductsContextData = {
     products,
     productsFiltered,
+    minPrice,
+    maxPrice,
+    brands,
+    storages,
+    bluetooths,
   };
 
   React.useEffect(() => {
     if (LoadingStates.SUCCESS === statusProducts) {
       setProductsFiltered(products);
       // Extract data
-      const brands = findAllBrands(products);
-      const storages = findAllStorages(products);
-      const bluetooths = findAllBluetooths(products);
-
-      console.log('🚀 ~ React.useEffect ~ brands:', brands);
-      console.log('🚀 ~ React.useEffect ~ storages:', storages);
-      console.log('🚀 ~ React.useEffect ~ bluetooths:', bluetooths);
-
-      /*
-      const min = getMinPricePossible(products);
-      const max = getMaxPricePossible(products);
-      const storages = findAllStorages(products);
-      const bluetooths = findAllBluetooths(products);
-      console.log({
-        max,
-        min,
-        brands,
-        storages,
-        bluetooths,
-      });
-      */
+      setMinPrice(getMinPricePossible(products));
+      setMaxPrice(getMaxPricePossible(products));
+      setBrands(findAllBrands(products));
+      setStorages(findAllStorages(products));
+      setBluetooths(findAllBluetooths(products));
     }
-    console.log('🚀 ~ React.useEffect ~ products:', products);
-    console.log('🚀 ~ React.useEffect ~ products:', products);
   }, [statusProducts, products]);
 
   return (
