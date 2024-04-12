e6792fe2a39fa9419b11129251b06e58d5ea624b
Alejandro Coca <alxmcr@outlook.com>
2024-04-12 01:30:14 -0400

    wip

--- src/components/forms/FormFilterByRangePrices.tsx
+++ src/components/forms/FormFilterByRangePrices.tsx
@@ -2,37 +2,14 @@ import React from 'react';
 import Icon16x16ArrowRight from '../@icons/16x16/Icon16x16ArrowRight';
 import GroupInputsRangePrices from '../groups-inputs/GroupInputsRangePrices';
 
-type Props = {
-  minValue: number;
-  maxValue: number;
-  minPrice: number;
-  maxPrice: number;
-  setMinPrice: React.Dispatch<React.SetStateAction<number>>;
-  setMaxPrice: React.Dispatch<React.SetStateAction<number>>;
-};
-
-export default function FormFilterByRangePrices({
-  minPrice = 0,
-  maxPrice = 0,
-  minValue = 0,
-  maxValue = 0,
-  setMinPrice,
-  setMaxPrice,
-}: Props) {
+export default function FormFilterByRangePrices() {
   const onSubmit = (ev: React.FormEvent) => {
     ev.preventDefault();
   };
 
   return (
     <form onSubmit={onSubmit} className="flex items-start gap-3">
-      <GroupInputsRangePrices
-        minPrice={minPrice}
-        maxPrice={maxPrice}
-        minValue={minValue}
-        maxValue={maxValue}
-        setMinPrice={setMinPrice}
-        setMaxPrice={setMaxPrice}
-      />
+      <GroupInputsRangePrices />
       <div>
         <button
           type="submit"
