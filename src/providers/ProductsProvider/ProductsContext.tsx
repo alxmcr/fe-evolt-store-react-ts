0d53b1bf2c38a55e5f018b28955a8410461eb267
Alejandro Coca <alxmcr@outlook.com>
2024-04-12 23:53:25 -0400

    wip

--- src/providers/ProductsProvider/ProductsContext.tsx
+++ src/providers/ProductsProvider/ProductsContext.tsx
@@ -5,12 +5,8 @@ import { LoadingStates } from '../../@enums/appEnums';
 const initialState: ProductsContextData = {
   statusProducts: LoadingStates.IDLE,
   products: [],
-  productsFiltered: [],
   startRangePrice: 0,
   endRangePrice: 0,
-  brands: [],
-  bluetooths: [],
-  storages: [],
 };
 
 export const ProductsContext = React.createContext(initialState);
