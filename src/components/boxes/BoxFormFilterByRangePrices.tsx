d851cbda859359858eeadeb513ed2741d7e98368
Alejandro Coca <alxmcr@outlook.com>
2024-04-14 22:18:54 -0400

    wip

--- src/components/boxes/BoxFormFilterByRangePrices.tsx
+++ src/components/boxes/BoxFormFilterByRangePrices.tsx
@@ -1,18 +1,8 @@
-import React from 'react';
-import { FilterCriteriasContext } from '../../@providers/FilterCriteriasProvider/FilterCriteriasContext';
-import { formatNumber } from '../../helpers/helpers-format';
 import FormFilterByRangePrices from '../forms/FormFilterByRangePrices';
 
 export default function BoxFormFilterByRangePrices() {
-  const contextFilterCriterias = React.useContext(FilterCriteriasContext);
-
   return (
     <div className="flex flex-col gap-2">
-      <div className="flex gap-2 ">
-        <span>$ {formatNumber(contextFilterCriterias.minPrice)}</span>
-        <span>-</span>
-        <span>$ {formatNumber(contextFilterCriterias.maxPrice)}</span>
-      </div>
       <FormFilterByRangePrices />
     </div>
   );
