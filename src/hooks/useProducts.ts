ed82b6a145e2565e79432f8054cbba74a0a02cb7
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2024-04-15 18:03:17 -0400

    refactor/use-products-hook (#30)

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,17 +1,22 @@
 import React from 'react';
-import { MOCK_DATA_COMPUTERS } from '../mocks/mock-laptops';
-import { ProductData } from '../@types/appTypes';
 import { LoadingStates } from '../@enums/appEnums';
+import { ProductData } from '../@types/appTypes';
+import { findProductCheapest, findProductMostExpensive } from '../helpers/helpers-hooks';
+import { MOCK_DATA_COMPUTERS } from '../mocks/mock-laptops';
 
 export default function useProducts() {
   const [products, setProducts] = React.useState<ProductData[]>([]);
+  const [productCheapest, setProductCheapest] = React.useState<ProductData | null>(null);
+  const [productMostExpensive, setProductMostExpensive] = React.useState<ProductData | null>(null);
   const [statusProducts, setStatusProducts] = React.useState(LoadingStates.IDLE);
 
   React.useEffect(() => {
     setStatusProducts(LoadingStates.PENDING);
     setProducts(MOCK_DATA_COMPUTERS);
+    setProductCheapest(findProductCheapest(MOCK_DATA_COMPUTERS));
+    setProductMostExpensive(findProductMostExpensive(MOCK_DATA_COMPUTERS));
     setStatusProducts(LoadingStates.SUCCESS);
   }, []);
 
-  return { products, statusProducts, setProducts };
+  return { products, productCheapest, productMostExpensive, statusProducts, setProducts };
 }
