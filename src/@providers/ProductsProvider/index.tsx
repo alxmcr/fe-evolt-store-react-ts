ac8014c3abe5880bfe39bb3196b4fbc718d840ec
Alejandro Coca <alxmcr@outlook.com>
2024-04-15 16:31:06 -0400

    wip

--- src/@providers/ProductsProvider/index.tsx
+++ src/@providers/ProductsProvider/index.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { LoadingStates } from '../../@enums/appEnums';
 import { ProductsLimitsPrice } from '../../@types/reducerTypes';
-import { getMaxPricePossible, getMinPricePossible } from '../../helpers/helpers-products';
+import { findProductCheapest, findProductMostExpensive } from '../../helpers/helpers-hooks';
 import useProducts from '../../hooks/useProducts';
-import {
-  ProductsContext,
-  ProductsLimitsPriceContext,
-  ProductsLoadingStateContext,
-} from './ProductsContext';
+import { ProductsContext, ProductsLimitsPriceContext, ProductsLoadingStateContext } from './ProductsContext';
 
 type Props = {
   children: React.ReactNode;
@@ -26,8 +22,16 @@ export default function ProductsProvider({ children }: Props) {
   React.useEffect(() => {
     if (LoadingStates.SUCCESS === statusProducts) {
       // Find out limits price
-      setStartPrice(Math.floor(getMinPricePossible(products)));
-      setEndPrice(Math.floor(getMaxPricePossible(products)));
+      const productCheapest = findProductCheapest(products);
+      const productMostExpensive = findProductMostExpensive(products);
+      console.log("🚀 ~ React.useEffect ~ products:", products)
+      console.log('🚀 ~ React.useEffect ~ productCheapest:', productCheapest);
+      console.log('🚀 ~ React.useEffect ~ productMostExpensive:', productMostExpensive);
+
+      if (productCheapest !== null && productMostExpensive !== null) {
+        setStartPrice(Math.floor(productCheapest.priceValue));
+        setEndPrice(Math.floor(productMostExpensive.priceValue));
+      }
     }
   }, [statusProducts, products]);
 
--- src/helpers/helpers-hooks.ts
+++ src/helpers/helpers-hooks.ts
@@ -4,8 +4,23 @@ export const findProductCheapest = (products: ProductData[] = []) => {
   if (products.length === 0) return null;
 
   return products.reduce((possibleCheapestProduct: ProductData | null, currentProduct) => {
-    if (possibleCheapestProduct !== null && currentProduct.priceValue < possibleCheapestProduct.priceValue) {
+    if (possibleCheapestProduct !== null && possibleCheapestProduct.priceValue < currentProduct.priceValue) {
       return possibleCheapestProduct;
+    }
+
+    return currentProduct;
+  }, null);
+};
+
+export const findProductMostExpensive = (products: ProductData[] = []) => {
+  if (products.length === 0) return null;
+
+  return products.reduce((possibleMostExpensiveProduct: ProductData | null, currentProduct) => {
+    if (
+      possibleMostExpensiveProduct !== null &&
+      possibleMostExpensiveProduct.priceValue > currentProduct.priceValue
+    ) {
+      return possibleMostExpensiveProduct;
     }
 
     return currentProduct;
