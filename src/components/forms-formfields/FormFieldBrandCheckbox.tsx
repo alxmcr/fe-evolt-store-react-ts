b61631967cb2f664f9e7436d9f916d2586b2cc9e
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2024-04-15 18:37:52 -0400

    fix/checkbox-icon (#32)

--- src/components/forms-formfields/FormFieldBrandCheckbox.tsx
+++ src/components/forms-formfields/FormFieldBrandCheckbox.tsx
@@ -54,7 +54,7 @@ export default function FormFieldBrandCheckbox({ brand }: Props) {
       />
       <span
         className="flex items-center gap-2 before:flex before:size-5 before:items-center before:justify-center before:rounded-full before:border before:border-solid before:border-perano-500 before:content-[''] peer-checked/draft:text-perano-500  peer-checked/draft:before:bg-perano-500
-        peer-checked/draft:before:bg-[url(icons/16x16/Icon16x16Check.svg)] peer-checked/draft:before:bg-center peer-checked/draft:before:bg-no-repeat"
+        peer-checked/draft:before:bg-[url(/icons/16x16/Icon16x16Check.svg)] peer-checked/draft:before:bg-center peer-checked/draft:before:bg-no-repeat"
       >
         {brand.name}
       </span>
