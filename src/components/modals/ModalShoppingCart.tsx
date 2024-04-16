370927c73e7891da411575d5801a11c1b39484d2
Alejandro Coca <alxmcr@outlook.com>
2024-04-16 01:38:31 -0400

    wip

--- src/components/modals/ModalShoppingCart.tsx
+++ src/components/modals/ModalShoppingCart.tsx
@@ -1,18 +1,18 @@
-import Icon10x10Close from '../@icons/10x10/Icon10x10Close';
-import Icon16x16ArrowForward from '../@icons/16x16/Icon16x16ArrowForward';
+import Icon18x18ArrowForward from '../@icons/18x18/Icon18x18ArrowForward';
+import Icon18x18Close from '../@icons/18x18/Icon18x18Close';
 import BoxProductsInCart from '../boxes/BoxProductsInCart';
 
 export default function ModalShoppingCart() {
   return (
-    <article className="flex flex-col gap-10 bg-light-50 px-[8px] py-[12px]">
+    <article className="flex w-[18.75rem] flex-col gap-10 bg-light-50 px-[8px] py-[12px]">
       <header className="flex items-center justify-between">
         <h3 className="font-semibold">Shopping cart</h3>
-        <Icon10x10Close />
+        <Icon18x18Close />
       </header>
       <BoxProductsInCart />
       <button className="flex h-[2.625rem] items-center justify-center gap-4 rounded-lg bg-perano-300">
         <span>Checkout</span>
-        <Icon16x16ArrowForward />
+        <Icon18x18ArrowForward />
       </button>
     </article>
   );
