6f9c3041185ca173b18daa940d3ccfbcfecbb56e
Alejandro Coca <alxmcr@outlook.com>
2024-04-17 01:19:25 -0400

    wip

--- src/components/boxes/BoxProductsInCart.tsx
+++ src/components/boxes/BoxProductsInCart.tsx
@@ -1,4 +1,6 @@
 import { ProductInCart } from '../../@types/shoppingCartTypes';
+import { formatPriceWithDecimals } from '../../helpers/helpers-format';
+import { calculateTotalToPayOnCart } from '../../helpers/helpers-products-in-cart';
 import Icon18x18ArrowForward from '../@icons/18x18/Icon18x18ArrowForward';
 import Icon200x200Cart from '../@icons/200x200/Icon200x200Cart';
 import StackProductsInCart from '../stacks/StackProductsInCart';
@@ -8,6 +10,9 @@ type Props = {
 };
 
 export default function BoxProductsInCart({ productsInCart = [] }: Props) {
+  const totalToPay = calculateTotalToPayOnCart(productsInCart);
+  const totalToPayStr = formatPriceWithDecimals(totalToPay);
+
   const handleGoToCheckout = () => {
     console.log('go to checkout');
   };
@@ -27,7 +32,7 @@ export default function BoxProductsInCart({ productsInCart = [] }: Props) {
       <div className="flex h-[100px] w-full flex-col items-center justify-between gap-4 px-[12px]">
         <div className="flex w-full items-center justify-between">
           <h4>Total</h4>
-          <span className="text-[1.25rem] font-bold text-perano-600">MXN 5999.97</span>
+          <span className="text-[1.25rem] font-bold text-perano-600">MXN {totalToPayStr}</span>
         </div>
         <button
           className="flex h-[2.625rem] w-full items-center justify-center gap-4 rounded-lg bg-perano-600 text-light-50"
