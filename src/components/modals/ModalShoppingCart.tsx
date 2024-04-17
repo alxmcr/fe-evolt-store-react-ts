91c318881a61d71577c89ebfd3365492ff05a34d
Alejandro Coca <alxmcr@outlook.com>
2024-04-17 17:40:24 -0400

    wip

--- src/components/modals/ModalShoppingCart.tsx
+++ src/components/modals/ModalShoppingCart.tsx
@@ -15,7 +15,7 @@ export default function ModalShoppingCart() {
   return (
     <dialog
       ref={dialogRef}
-      className="w-[304px] bg-light-50 backdrop:bg-black/50 md:right-[-380px] md:top-[94px] md:w-[336px] lg:right-[-610px] lg:top-[-190px] lg:w-[392px]"
+      className="w-[304px] bg-light-50 backdrop:bg-black/50 md:right-[-380px] md:top-[53px] md:w-[336px] lg:right-[-640px] lg:top-[70px] xl:right-[-610px] xl:top-[-190px] xl:w-[392px]"
     >
       <div className="flex flex-col gap-6 ">
         <header className="flex items-center justify-between border border-b-perano-400 p-[12px]">
