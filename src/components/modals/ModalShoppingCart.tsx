5ee1e36b7435d3e4b9012f163ba70bc04b41a885
Alejandro Coca <alxmcr@outlook.com>
2024-04-17 00:50:03 -0400

    wip

--- src/components/modals/ModalShoppingCart.tsx
+++ src/components/modals/ModalShoppingCart.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import { ShoppingCartContext } from '../../@providers/ShoppingCartProvider/ShoppingCardContext';
 import Icon18x18Close from '../@icons/18x18/Icon18x18Close';
 import BoxProductsInCart from '../boxes/BoxProductsInCart';
-import { MOCK_PRODUCTS_IN_CART } from '../../mocks/mock-products-in-cart';
 
 export default function ModalShoppingCart() {
   const shoppingCart = React.useContext(ShoppingCartContext);
-  console.log(shoppingCart.productsInCart);
 
   return (
     <article className="flex w-[304px] flex-col gap-6 bg-light-50 md:w-[336px] lg:w-[392px]">
@@ -16,7 +14,7 @@ export default function ModalShoppingCart() {
         </h3>
         <Icon18x18Close />
       </header>
-      <BoxProductsInCart productsInCart={MOCK_PRODUCTS_IN_CART} />
+      <BoxProductsInCart productsInCart={shoppingCart.productsInCart} />
     </article>
   );
 }
