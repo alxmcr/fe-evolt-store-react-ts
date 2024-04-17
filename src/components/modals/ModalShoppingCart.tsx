ce5abe58ee7f9956844418bd607944ff52b5c367
Alejandro Coca <alxmcr@outlook.com>
2024-04-17 16:07:08 -0400

    wip

--- src/components/modals/ModalShoppingCart.tsx
+++ src/components/modals/ModalShoppingCart.tsx
@@ -11,16 +11,18 @@ export default function ModalShoppingCart() {
   };
 
   return (
-    <article className="flex w-[304px] flex-col gap-6 bg-light-50 md:w-[336px] lg:w-[392px]">
-      <header className="flex items-center justify-between border border-b-perano-400 p-[12px]">
-        <h3 className="text-[18px] font-bold uppercase tracking-widest text-perano-600 md:text-[26px] lg:text-[28px]">
-          Shopping cart
-        </h3>
-        <button onClick={handleCloseModal}>
-          <Icon18x18Close />
-        </button>
-      </header>
-      <BoxProductsInCart productsInCart={shoppingCart.productsInCart} />
+    <article className="flex w-[304px] flex-col gap-6 bg-light-50 backdrop:bg-black/50 md:w-[336px] lg:w-[392px]">
+      <div className="flex flex-col gap-6 ">
+        <header className="flex items-center justify-between border border-b-perano-400 p-[12px]">
+          <h3 className="text-[18px] font-bold uppercase tracking-widest text-perano-600 md:text-[26px] lg:text-[28px]">
+            Shopping cart
+          </h3>
+          <button onClick={handleCloseModal}>
+            <Icon18x18Close />
+          </button>
+        </header>
+        <BoxProductsInCart productsInCart={shoppingCart.productsInCart} />
+      </div>
     </article>
   );
 }
