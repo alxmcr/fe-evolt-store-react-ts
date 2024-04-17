89b1cf5eb38d3696a6eeebda0cfaf70ad7b01eec
Alejandro Coca <alxmcr@outlook.com>
2024-04-17 00:35:58 -0400

    wip

--- src/components/boxes/BoxProductsInCart.tsx
+++ src/components/boxes/BoxProductsInCart.tsx
@@ -18,9 +18,9 @@ export default function BoxProductsInCart({ productsInCart = [] }: Props) {
   }
 
   return (
-    <div className="flex h-[31.25rem] flex-col items-center gap-6">
+    <div className="flex h-[550px] flex-col items-center gap-6">
       <StackProductsInCart productsInCart={productsInCart} />
-      <div className="flex h-[156px] w-full flex-col items-center justify-between gap-4 px-[12px]">
+      <div className="flex h-[100px] w-full flex-col items-center justify-between gap-4 px-[12px]">
         <div className="flex w-full items-center justify-between">
           <h4>Total</h4>
           <span className="text-[1.25rem] font-bold text-perano-600">MXN 5999.97</span>
--- src/components/stacks/StackProductsInCart.tsx
+++ src/components/stacks/StackProductsInCart.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function StackProductsInCart({ productsInCart = [] }: Props) {
   return (
-    <div className="scrollbar-app flex h-[310px] w-full flex-col gap-5 overflow-y-auto px-[8px] pb-[16px]">
+    <div className="scrollbar-app flex h-[410px] w-full flex-col gap-5 overflow-y-auto px-[8px] pb-[16px]">
       {productsInCart.map((productInCart) => (
         <CardProductInCart key={productInCart.id} productInCart={productInCart} />
       ))}
