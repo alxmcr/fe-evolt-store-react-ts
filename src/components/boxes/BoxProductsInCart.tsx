17556bcf7e58c5f4acd2346bc523cd7e535289d3
Alejandro Coca <alxmcr@outlook.com>
2024-04-16 23:39:33 -0400

    wip

--- src/components/boxes/BoxProductsInCart.tsx
+++ src/components/boxes/BoxProductsInCart.tsx
@@ -10,7 +10,7 @@ type Props = {
 export default function BoxProductsInCart({ productsInCart = [] }: Props) {
   if (productsInCart.length === 0) {
     return (
-      <div className="flex h-[31.25rem] flex-col items-center justify-center gap-6 bg-[#f6bcf6]">
+      <div className="flex h-[31.25rem] flex-col items-center justify-center gap-6">
         <Icon200x200Cart />
         <p>Your shopping list is empty.</p>
       </div>
@@ -18,14 +18,14 @@ export default function BoxProductsInCart({ productsInCart = [] }: Props) {
   }
 
   return (
-    <div className="flex h-[31.25rem] flex-col items-center gap-6 bg-[#f6bcf6]">
+    <div className="flex h-[31.25rem] flex-col items-center gap-6">
       <StackProductsInCart productsInCart={productsInCart} />
       <div className="flex h-[156px] w-full flex-col items-center justify-between gap-4 px-[12px]">
         <div className="flex w-full items-center justify-between">
           <h4>Total</h4>
           <span className="text-[1.25rem] font-bold text-perano-600">MXN 5999.97</span>
         </div>
-        <button className="flex h-[2.625rem] w-full items-center justify-center gap-4 rounded-lg bg-perano-300">
+        <button className="flex h-[2.625rem] w-full items-center justify-center gap-4 rounded-lg bg-perano-600 text-light-50">
           <span>Checkout</span>
           <Icon18x18ArrowForward />
         </button>
--- src/components/modals/ModalShoppingCart.tsx
+++ src/components/modals/ModalShoppingCart.tsx
@@ -11,7 +11,9 @@ export default function ModalShoppingCart() {
   return (
     <article className="flex w-[304px] flex-col gap-6 bg-light-50 md:w-[336px] lg:w-[392px]">
       <header className="flex items-center justify-between border border-b-perano-400 p-[12px]">
-        <h3 className="font-semibold">Shopping cart</h3>
+        <h3 className="text-[18px] font-bold uppercase tracking-widest text-perano-600 md:text-[26px] lg:text-[28px]">
+          Shopping cart
+        </h3>
         <Icon18x18Close />
       </header>
       <BoxProductsInCart productsInCart={MOCK_PRODUCTS_IN_CART} />
