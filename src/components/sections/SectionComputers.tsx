9cd1dfbc6cbdb1db60ef8792e53c4256487b7f35
Alejandro Coca <alxmcr@outlook.com>
2024-04-18 02:08:27 -0400

    wip

--- src/components/sections/SectionComputers.tsx
+++ src/components/sections/SectionComputers.tsx
@@ -1,10 +1,14 @@
+import React from 'react';
+import { ProductsContext } from '../../@providers/ProductsProvider/ProductsContext';
 import AsideProductsFilter from '../asides/AsideProductsFilter';
 import BoxGridProducts from '../boxes/BoxGridProducts';
 
 export default function SectionComputers() {
+  const products = React.useContext(ProductsContext);
+
   return (
     <section className="flex flex-col gap-4">
-      <h2 className="text-xl font-bold">Computers (0)</h2>
+      <h2 className="text-xl font-bold">Computers ({products.length})</h2>
       <div className="lg:relative lg:flex lg:gap-4">
         <AsideProductsFilter />
         <BoxGridProducts />
