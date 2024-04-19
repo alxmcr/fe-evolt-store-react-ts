df7945f80948a369166d739b49359f6b9167069c
Alejandro Coca <alxmcr@outlook.com>
2024-04-19 01:46:47 -0400

    wip

--- src/components/boxes/boxes-images/BoxProductImageCover.tsx
+++ src/components/boxes/boxes-images/BoxProductImageCover.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export default function BoxProductImageCover({ photoFilename = '', photoAlt = '' }: Props) {
   return (
-    <div className="flex size-[100px] flex-col justify-center bg-perano-300 md:size-[120px] lg:size-[135px]">
+    <div className="flex size-[94px] flex-col justify-center bg-perano-300 md:size-[120px] lg:size-[135px]">
       <img src={`/product-assets/${photoFilename}`} alt={photoAlt} className="object-contain" />
     </div>
   );
