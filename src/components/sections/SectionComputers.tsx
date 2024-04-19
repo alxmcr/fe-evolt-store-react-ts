5503504f7c070aed99750e34be9e4fe1a932ef7e
Alejandro Coca <alxmcr@outlook.com>
2024-04-18 22:26:10 -0400

    wip

--- src/components/sections/SectionComputers.tsx
+++ src/components/sections/SectionComputers.tsx
@@ -6,6 +6,14 @@ import BoxGridProducts from '../boxes/BoxGridProducts';
 export default function SectionComputers() {
   const products = React.useContext(ProductsContext);
 
+  if (products?.length === 0) {
+    return (
+      <div className="flex h-[88vh] w-full flex-col items-center justify-center gap-3 bg-white">
+        <h2 className="w-[90%] text-center text-[2rem]">Your tech store does not have products.</h2>
+      </div>
+    );
+  }
+
   return (
     <section className="flex flex-col gap-4">
       <h2 className="text-xl font-bold">Computers ({products.length})</h2>
