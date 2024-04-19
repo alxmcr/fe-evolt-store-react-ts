fdcfe0bf63d0f9b9ed633d29d2cf5a198dae16cd
Alejandro Coca <alxmcr@outlook.com>
2024-04-19 01:21:01 -0400

    wip

--- src/components/boxes/BoxGridProducts.tsx
+++ src/components/boxes/BoxGridProducts.tsx
@@ -1,13 +1,59 @@
 import React from 'react';
-import { ProductsContext } from '../../@providers/ProductsProvider/ProductsContext';
+import { FilterCriteriasDispatchContext } from '../../@providers/FilterCriteriasProvider/FilterCriteriasContext';
+import {
+  FilterProductsTagsContext,
+  FilterProductsTagsDispatchContext,
+} from '../../@providers/TagsFilterProvider/TagsFilterContext';
+import { ProductData } from '../../@types/appTypes';
+import Icon200x200BugGameIcon from '../@icons/200x200/Icon200x200BugGameIcon';
 import GridProducts from '../grids/GridProducts';
+import BoxFilterActions from './BoxFilterActions';
 
-export default function BoxGridProducts() {
-  const products = React.useContext(ProductsContext);
+type Props = {
+  productsFiltered: ProductData[];
+};
+
+export default function BoxGridProducts({ productsFiltered = [] }: Props) {
+  const tagsFilter = React.useContext(FilterProductsTagsContext);
+
+  const dipatchFilterCriterias = React.useContext(FilterCriteriasDispatchContext);
+  const dispatchTagsFilter = React.useContext(FilterProductsTagsDispatchContext);
+
+  const resetFilter = () => {
+    console.log('reset tags...');
+    // Reset filter criterias
+    dipatchFilterCriterias({ type: 'reset_filter_criterias' });
+
+    // Reset tags
+    dispatchTagsFilter({ type: 'reset_tags_filter' });
+  };
+
+  if (tagsFilter?.length > 0 && productsFiltered?.length === 0) {
+    return (
+      <div className="flex w-full flex-col gap-4 lg:w-4/6">
+        {tagsFilter.length > 0 ? <BoxFilterActions /> : null}
+        <section className="flex w-full flex-col gap-4">
+          <div className="flex h-[88vh] w-full flex-col items-center justify-center gap-3 bg-white">
+            <span className="text-perano-600">
+              <Icon200x200BugGameIcon />
+            </span>
+            <h2 className="w-[90%] text-center text-[2rem]">Filter returned not results.</h2>
+            <button
+              className="flex min-h-10 w-[200px] items-center justify-center gap-2 rounded-lg border border-perano-800 px-3 py-4 text-[1.5rem] text-perano-800 md:hover:border-[#6691FF] md:hover:bg-[#6691FF] md:hover:text-light-50"
+              onClick={resetFilter}
+            >
+              Reset filter
+            </button>
+          </div>
+        </section>
+      </div>
+    );
+  }
 
   return (
     <div className="flex w-full flex-col gap-4 lg:w-4/6">
-      <GridProducts products={products} />
+      {tagsFilter.length > 0 ? <BoxFilterActions /> : null}
+      <GridProducts products={productsFiltered} />
     </div>
   );
 }
--- src/components/sections/SectionComputers.tsx
+++ src/components/sections/SectionComputers.tsx
@@ -1,39 +1,18 @@
 import React from 'react';
-import {
-  FilterCriteriasContext,
-  FilterCriteriasDispatchContext,
-} from '../../@providers/FilterCriteriasProvider/FilterCriteriasContext';
+import { FilterCriteriasContext } from '../../@providers/FilterCriteriasProvider/FilterCriteriasContext';
 import { ProductsContext } from '../../@providers/ProductsProvider/ProductsContext';
-import {
-  FilterProductsTagsContext,
-  FilterProductsTagsDispatchContext,
-} from '../../@providers/TagsFilterProvider/TagsFilterContext';
 import { ProductData } from '../../@types/appTypes';
 import { applyFilterCriterias } from '../../helpers/helpers-tags-filter';
 import Icon200x200BugGameIcon from '../@icons/200x200/Icon200x200BugGameIcon';
 import AsideProductsFilter from '../asides/AsideProductsFilter';
-import BoxFilterActions from '../boxes/BoxFilterActions';
 import BoxGridProducts from '../boxes/BoxGridProducts';
 
 export default function SectionComputers() {
   const products = React.useContext(ProductsContext);
   const filterProductsCriterias = React.useContext(FilterCriteriasContext);
-  const tagsFilter = React.useContext(FilterProductsTagsContext);
-  const dipatchFilterCriterias = React.useContext(FilterCriteriasDispatchContext);
-  const dispatchTagsFilter = React.useContext(FilterProductsTagsDispatchContext);
-
   // apply filter
   // const productsFiltered: ProductData[] = []
   const productsFiltered: ProductData[] = applyFilterCriterias(products, filterProductsCriterias);
-
-  const resetFilter = () => {
-    console.log('reset tags...');
-    // Reset filter criterias
-    dipatchFilterCriterias({ type: 'reset_filter_criterias' });
-
-    // Reset tags
-    dispatchTagsFilter({ type: 'reset_tags_filter' });
-  };
 
   if (products?.length === 0) {
     return (
@@ -46,32 +25,12 @@ export default function SectionComputers() {
     );
   }
 
-  if (tagsFilter?.length > 0 && productsFiltered?.length === 0) {
-    return (
-      <section className="flex w-full flex-col gap-4">
-        {tagsFilter.length > 0 ? <BoxFilterActions /> : null}
-        <div className="flex h-[88vh] w-full flex-col items-center justify-center gap-3 bg-white">
-          <span className="text-perano-600">
-            <Icon200x200BugGameIcon />
-          </span>
-          <h2 className="w-[90%] text-center text-[2rem]">Filter returned not results.</h2>
-          <button
-            className="flex min-h-10 w-[200px] items-center justify-center gap-2 rounded-lg border border-perano-800 px-3 py-4 text-[1.5rem] text-perano-800 md:hover:border-[#6691FF] md:hover:bg-[#6691FF] md:hover:text-light-50"
-            onClick={resetFilter}
-          >
-            Reset filter
-          </button>
-        </div>
-      </section>
-    );
-  }
-
   return (
     <section className="flex w-full flex-col gap-4">
-      <h2 className="text-xl font-bold">Computers ({productsFiltered.length})</h2>
+      <h2 className="text-xl font-bold">Products ({productsFiltered.length})</h2>
       <div className="lg:relative lg:flex lg:gap-4">
         <AsideProductsFilter />
-        <BoxGridProducts />
+        <BoxGridProducts productsFiltered={productsFiltered} />
       </div>
     </section>
   );
